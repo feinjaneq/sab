f0fac829150b63796edf639fea270c95aa4d955d
feinjaneq <niesam928@gmail.com>
2025-11-02 20:53:51 +0100

    Create aj.lua

---
+++ aj.lua
@@ -0,0 +1,11 @@
+if syn and syn.queue_on_teleport then
+    syn.queue_on_teleport([[
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/feinjaneq/sab/refs/heads/main/ajmain.lua", true))()
+    ]])
+elseif queue_on_teleport then
+    queue_on_teleport([[
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/feinjaneq/sab/refs/heads/main/ajmain.lua", true))()
+    ]])
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/feinjaneq/sab/refs/heads/main/ajmain.lua",true))()
